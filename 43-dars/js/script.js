75026aba3962f311930147a0da14cb3def21e712
eldor <eldorodilov2@gmail.com>
2022-03-24 00:45:37 +0500

    add

--- 43-dars/js/script.js
+++ 43-dars/js/script.js
@@ -13,11 +13,204 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabContent = document.querySelectorAll(".tabcontent"),
-    headerParents = document.querySelector(".tabheader_items");
+    headerParents = document.querySelector(".tabheader__items");
 
   function hideTabContent() {
     tabContent.forEach((item) => {
       item.style.display = "none";
     });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
   }
+
+  function showTabContent(i = 0) {
+    tabContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+  headerParents.addEventListener("click", (event) => {
+    // console.log(event);
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  //============== MODAL =======================//
+  const allModal = document.querySelectorAll("[data-modal"),
+    modal = document.querySelector(".modal"),
+    modalClose = document.querySelector("[data-close]");
+
+  allModal.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimer);
+  }
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modalClose.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      closeModal();
+    }
+  });
+
+  const modalTimer = setTimeout(openModal, 5000);
+
+  function showMyModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showMyModalByScroll);
+    }
+  }
+  window.addEventListener("scroll", showMyModalByScroll);
+
+  //  DATA
+
+  const deadLine = "2022-03-24";
+
+  function getTime(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((total / 1000) % 60),
+      minutes = Math.floor((total / (1000 * 60)) % 60),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 60);
+    return {
+      total: total,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = document.querySelector("#days"),
+      hours = document.querySelector("#hours"),
+      minutes = document.querySelector("#minutes"),
+      seconds = document.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000); // Har sekuntda takrorlanadi
+
+    updateClock();
+
+    function updateClock() {
+      const time = getTime(endtime); // parametrlarimnni time uzgaruvchisiga olyapti
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadLine);
+
+  class CarCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 1000;
+      this.changeToUSD();
+    }
+
+    changeToUSD() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach((className) => {
+          element.classList.add(className);
+        });
+      }
+
+      element.innerHTML = `
+          <div class="menu__item">
+          <img src=${this.src} alt=${this.alt} />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Price:</div>
+            <div class="menu__item-total"><span>${this.price}</span> $</div>
+          </div>
+        </div>
+      `;
+
+      this.parent.append(element);
+    }
+  }
+
+  new CarCard(
+    "img/tabs/1.jpg",
+    "vegy",
+    "2021 Mercedes-Benz C-Class",
+    ` The 2021 Mercedes-Benz C-Class finishes in the top half of our
+    luxury small car rankings. It's powerful and upscale, but it has
+    so-so handli...`,
+    100,
+    ".menu .container"
+    // "red"
+  ).render();
+  new CarCard(
+    "img/tabs/2.jpg",
+    "car",
+    "2021 Mercedes-Benz CLA-Class",
+    ` The 2021 Mercedes-Benz CLA offers punchy powertrains, an elegant
+    interior, and easy-to-use tech features, but it also has a firm
+    ride and a ..`,
+    20,
+    ".menu .container"
+    // "black"
+  ).render();
+  new CarCard(
+    "img/tabs/3.jpg",
+    "vegy",
+    "2021 Mercedes-Benz SCLA",
+    ` The German luxury car-manufacturer has been around for more than a
+    century, having elegantly drifted rough curves of automobile.`,
+    40,
+    ".menu .container"
+  ).render();
 });
